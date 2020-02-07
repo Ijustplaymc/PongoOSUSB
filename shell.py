6119c87f74b064644cd686ca791762cfc66dd56a
machport <54745286+machport@users.noreply.github.com>
2020-02-07 07:04:41 -0800

    requires python 3

--- shell.py
+++ shell.py
@@ -1,19 +1,15 @@
 import sys
 import usb.core
 import readline
-if sys.version[0] != 3:
-	print("although this script works in Python 2 it will be unsupported for this script and will only use Python 3")
 dev = usb.core.find(idVendor=0x05ac, idProduct=0x4141)
 if dev is None:
     raise ValueError('Device not found')
 dev.set_configuration()
 while True:  # making a loop
-	if sys.version[0] == 3:
-		c = input("$:")
-	elif sys.version[0] == 2:
-		c = raw_input("$:")
+	c = input("$:")
 	if "/" == c[0]:
 		if c.split("/")[1].split(" ")[0] == "load":
+			dev.ctrl_transfer(0x21, 3, 0, 0, "fbclear\n")
 			data = open(c.split(" ")[1], "rb").read()
 			dev.ctrl_transfer(0x21, 2, 0, 0, 0)
 			dev.ctrl_transfer(0x21, 1, 0, 0, 0)
@@ -31,4 +27,4 @@ while True:  # making a loop
 			dev.ctrl_transfer(0x21, 3, 0, 0, "modload\n")
 			dev.ctrl_transfer(0x21, 3, 0, 0, "bootx\n")
 	else:
-		dev.ctrl_transfer(0x21, 3, 0, 0, c + "\n")	
+		dev.ctrl_transfer(0x21, 3, 0, 0, c + "\n")
